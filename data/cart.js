40c72ed923a88988293a28d58c1d7f241ee3875e
MinhDuyTN <tnmd130103@gmail.com>
2024-03-01 12:17:39 +0700

    Move addToCart to Cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-const cart = [];
+export const cart = [];
+
+export const AddToCart = (productId) => {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+};
