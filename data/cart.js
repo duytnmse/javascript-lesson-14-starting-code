5c2a82e60330eee1257d6c877dc516be897c39a8
MinhDuyTN <tnmd130103@gmail.com>
2024-03-02 12:10:21 +0700

    Fix price format and  radio selectors

--- data/cart.js
+++ data/cart.js
@@ -7,6 +7,10 @@ export const cart = [
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
   },
+  {
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity: 2,
+  },
 ];
 
 export const AddToCart = (productId) => {
