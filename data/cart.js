585a2f0fcc6ededb4365a8d446026e2e80371408
duytnmse <duytnmse173241@fpt.edu.vn>
2024-03-06 09:49:45 +0700

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,21 @@
+import { deliveryOptions } from "./deliveryOptions.js";
+
 export let cart = JSON.parse(localStorage.getItem("localStorageCart"));
 
+if (!cart || cart.length === 0) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
+  ];
+}
 function SaveToLocalStorage() {
   localStorage.setItem("localStorageCart", JSON.stringify(cart));
 }
@@ -19,6 +35,7 @@ export const AddToCart = (productId) => {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
   SaveToLocalStorage();
